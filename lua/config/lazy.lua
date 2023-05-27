5df0681d2789feae27085d27a9f8ed9fa8710150
Satria Alipatullah <satrialipatullah@gmail.com>
2023-05-27 16:34:16 +0700

    fix: replace dashboard with alpha

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,9 +17,12 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.ui.mini-starter" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.vscode" },
+    -- { import = "goolord/alpha-nvim" },
+    -- { import = "nanozuki/tabby.nvim" },
     -- import/override with your plugins
     { import = "plugins" },
   },
