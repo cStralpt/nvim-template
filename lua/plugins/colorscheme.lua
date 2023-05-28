147b83275be3856b3771b62df45cc503a143b99b
Satria Alipatullah <satrialipatullah@gmail.com>
2023-05-28 08:38:47 +0700

    add: some options

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,19 +5,18 @@ return {
   { "projekt0n/github-nvim-theme" },
   { "sainnhe/sonokai" },
   { "marko-cerovac/material.nvim" },
-  { "ray-x/aurora"},
-  { "rafamadriz/neon"},
-  { "nxvu699134/vn-night.nvim"},
-  { "lmburns/kimbox"},
-  
+  { "ray-x/aurora" },
+  { "rafamadriz/neon" },
+  { "nxvu699134/vn-night.nvim" },
+  { "lmburns/kimbox" },
 
   -- Configure your theme here
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "tokyonight", -- default tokyonight
-  --   },
-  -- },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark_dimmed", -- default tokyonight
+    },
+  },
   {
     "folke/tokyonight.nvim",
     opts = {
