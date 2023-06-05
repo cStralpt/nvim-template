1f5a88284fc9d0672ec1be25e2d7b984ca24379e
Satria Alipatullah <satrialipatullah@gmail.com>
2023-06-06 06:52:18 +0700

    add: some changes

--- init.lua
+++ init.lua
@@ -1,11 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 if vim.g.vscode then
   -- VSCode extension
+  require("config.lazy")
 else
   require("config.lazy")
   require("nvim-ts-autotag").setup()
   require("toggleterm").setup({})
   require("oil").setup()
+  require("notify").setup({
+    background_colour = "#222436",
+  })
   vim.api.nvim_command("highlight LineNr guifg=#bae67e ctermfg=149")
   vim.api.nvim_command("highlight CursorLineNr guifg=#ef6b73 ctermfg=203")
 end
