ba9b3129c426f7417dcb203aab18385426de6205
Satria Alipatullah <satrialipatullah@gmail.com>
2023-06-26 19:46:14 +0700

    add: some colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,21 @@
 return {
 
-  --   { "rebelot/kanagawa.nvim" },
-  --   { "projekt0n/github-nvim-theme" },
-  -- { "nxvu699134/vn-night.nvim" },
-  --   { "lmburns/kimbox" },
-  -- { "olimorris/onedarkpro.nvim" },
-  --   { "nyoom-engineering/oxocarbon.nvim" },
+  -- { "rebelot/kanagawa.nvim" },
+  -- { "projekt0n/github-nvim-theme" },
+  { "nxvu699134/vn-night.nvim" },
+  -- { "lmburns/kimbox" },
+  { "olimorris/onedarkpro.nvim" },
+  -- { "nyoom-engineering/oxocarbon.nvim" },
   -- { "navarasu/onedark.nvim" },
-  { "marko-cerovac/material.nvim" },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "tokynight-moon",
-  --   },
-  -- },
+  -- { "marko-cerovac/material.nvim" },
+  { "EdenEast/nightfox.nvim" },
+  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nightfly",
+    },
+  },
   {
     "folke/tokyonight.nvim",
     opts = {
